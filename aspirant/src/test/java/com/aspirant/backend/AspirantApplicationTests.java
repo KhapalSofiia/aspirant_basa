0753b61ce6c8ff8b47256ad1b0278a12bb70ae38
sofiia <sofiia.khapal@nure.ua>
2025-11-17 14:17:27 +0100

    create project

---
+++ aspirant/src/test/java/com/aspirant/backend/AspirantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirant.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspirantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
